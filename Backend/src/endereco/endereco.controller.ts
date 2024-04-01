f21f5493af136efe9feb6a65fdecbc87aaf70bcf
Enzo Bonfante <enzobonfx@gmail.com>
2024-04-01 16:20:53 -0300

    feature: CRUD de times

---
+++ Backend/src/endereco/endereco.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param, UseGuards, UsePipes } from '@nestjs/common';
+import { EnderecoService } from './endereco.service';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+
+@ApiTags('Endereco')
+@Controller('endereco')
+@UseGuards(AuthGuard())
+export class EnderecoController {
+	constructor(private readonly enderecoService: EnderecoService) {}
+
+	@Get('cep/:cep')
+	findByCep(@Param('cep') cep: string) {
+		return this.enderecoService.findByCep(cep);
+	}
+}
